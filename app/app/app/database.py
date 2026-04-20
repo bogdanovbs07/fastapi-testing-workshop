f512929eb664b054ef58588561e4f174046225bf
bogdanovbs07 <bogdanovbs07@yandex.ru>
2026-04-20 11:29:13 +0300

    Create database.py

---
+++ app/app/app/database.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+DATABASE_URL = "postgresql+asyncpg://postgres:1234@localhost:5432/Test"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_session():
+    async with AsyncSessionLocal() as session:
+        yield session
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
